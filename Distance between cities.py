4dd0f6c6e1f60091475c487777e7bf049ec1135e
Jack-Jieran Geng <89900002+Sbb86173319@users.noreply.github.com>
2021-09-15 08:32:50 +0800

    Add files via upload

---
+++ Distance between cities.py
@@ -0,0 +1,73 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Sep 15 08:19:31 2021
+
+@author: jrgen
+"""
+
+distance_dic={}
+distance_dic[6667] = {'Oak Park（Illinois）','Paris'}
+distance_dic[7307] = {'Milano','Oak Park（Illinois）'}
+distance_dic[712] = {'Oak Park（Illinois）','Toronto'}
+distance_dic [12] ={'Oak Park（Illinois）','Chicago'}
+distance_dic[2007] = {'Oak Park（Illinois）','Key West'}
+distance_dic[2147] = {'Oak Park（Illinois）','Havana'}
+distance_dic[7505] = {'Oak Park（Illinois）','Venice'}
+distance_dic[12464] = {'Oak Park（Illinois）','Entebbe'}
+distance_dic[1158] = {'Oak Park（Illinois）','New York'}
+distance_dic[2171] = {'Oak Park（Illinois）','Ketchum'}
+
+distance_dic [640] ={'Paris','Milano'}
+distance_dic[6006] = {'Paris','Toronto'}
+distance_dic[6661] = {'Paris','Chicago'}
+distance_dic[7573] = {'Paris','Key West'}
+distance_dic [7729] ={'Paris','Havana'}
+distance_dic[845] = {'Paris','Venice'}
+distance_dic[6149] = {'Paris','Entebbe'}
+distance_dic[5844] = {'Paris','New York'}
+distance_dic[8034] = {'Paris','Ketchum'}
+
+distance_dic [6644] ={'Milano','Toronto'}
+distance_dic[7301] = {'Milano','Chicago'}
+distance_dic[8173] = {'Milano','Key West'}
+distance_dic[8326] ={'Milano','Havana'}
+distance_dic [245] ={'Milano','Venice'}
+distance_dic[5546] = {'Milano','Entebbe'}
+distance_dic[6471] = {'Milano','New York'}
+distance_dic[8657] = {'Milano','Ketchum'}
+
+distance_dic [703] ={'Toronto','Chicago'}
+distance_dic [2142] ={'Toronto','Key West'}
+distance_dic [2306] ={'Toronto','Havana',}
+distance_dic [6849] ={'Toronto','Venice'}
+distance_dic [11752] ={'Toronto','Entebbe'}
+distance_dic [556] ={'Toronto','New York'}
+distance_dic [2792] ={'Toronto','Ketchum'}
+
+distance_dic [2000] ={'Chicago','Key West'}
+distance_dic [2140] ={'Chicago','Havana'}
+distance_dic [7499] ={'Chicago','Venice'}
+distance_dic [12455] ={'Chicago','Entebbe'}
+distance_dic [1147] ={'Chicago','New York'}
+distance_dic [2183] ={'Chicago','Ketchum'}
+
+distance_dic [170] ={'Key West','Havana'}
+distance_dic [8405] ={'Key West','Venice'}
+distance_dic [12455] ={'Key West','Entebbe'}
+distance_dic [1939] ={'Key West','New York'}
+distance_dic [3643] ={'Key West','Ketchum'}
+
+distance_dic [8559] ={'Havana','Venice'}
+distance_dic [12544] ={'Havana','Entebbe'}
+distance_dic [2109] ={'Havana','New York'}
+distance_dic [3711] ={'Havana','Ketchum'}
+
+
+distance_dic [5424] ={'Venice','Entebbe'}
+distance_dic [6688] ={'Venice','New York'}
+distance_dic [8807] ={'Venice','Ketchum'}
+
+distance_dic [11394] ={'Entebbe','New York'}
+distance_dic [14159] ={'Entebbe','Ketchum'}
+
+distance_dic [3312] ={'New York','Ketchum'}
